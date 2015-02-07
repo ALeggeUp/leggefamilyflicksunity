b3eef77c6fa8ed37c4f35e9fbbd3b2707d6d8220
Stephen Legge <stephen@aleggeup.com>
2015-02-07 14:30:05 -0500

    ApplicationConfig model to be injected where needed

---
+++ Assets/Scripts/Main/Model/ApplicationConfig.cs
@@ -0,0 +1,46 @@
+﻿/*
+ * ApplicationConfig.cs
+ * 
+ * Copyright (c) 2015 [ A Legge Up Consulting].  All Rights Reserved.
+ * 
+ * Unauthorized copying or redistribution of this file in whole or
+ * in part, or use in source and binary forms, with or without
+ * modification via any medium is strictly prohibited.
+ * 
+ * Proprietary and confidential.
+ * 
+ * Created: January 2015
+ * Author: Stephen Legge <stephen@aleggeup.com>
+ * 
+ * THIS IS UNPUBLISHED PROPRIETARY SOURCE CODE OF A LEGGE UP CONSULTING
+ * 
+ * The copyright notice above does not evidence any
+ * actual or intended publication of such source code.
+ */
+
+using UnityEngine;
+
+namespace aleggeup.leggefamilyflicks.main
+{
+    public class ApplicationConfig : IApplicationConfig
+    {
+        private static string APP_CONFIG = "JSON/AppConfig";
+
+        public string FlickrApplicationKey { get; private set; }
+        public string FlickrApplicationSecret { get; private set; }
+
+        //PostConstruct methods fire automatically after Construction
+        //and after all injections are satisfied. It's a safe place
+        //to do things you'd usually sonsider doing in the Constructor.
+        [PostConstruct]
+        public void PostConstruct ()
+        {
+            TextAsset file = Resources.Load (APP_CONFIG) as TextAsset;
+
+            var node = SimpleJSON.JSON.Parse (file.text);
+
+            FlickrApplicationKey = node ["FlickrApplicationKey"];
+            FlickrApplicationSecret = node ["FlickrApplicationSecret"];
+        }
+    }
+}
